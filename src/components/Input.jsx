877fcb3ca267e0f937c92ae1abce4027bb0ae312
Дмитрий <cudnovdima053@gmail.com>
2026-01-06 18:34:40 +0500

    Now you can add images.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../styles/input.css";
 
 export default function Input({ onSend }) {
   const [text, setText] = useState("");
+  const [ imagePreview, setImagePreview ] = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  const handlePickImage = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    if (!file.type.startsWith("image/")) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+
+    e.target.value = "";
+  }
 
   const handleSend = () => {
     if (!text.trim()) return;
@@ -12,6 +33,17 @@ export default function Input({ onSend }) {
 
   return (
     <div className="input-box">
+      {imagePreview && (
+        <div className="image-preview">
+          <img src={imagePreview} alt="preview" />
+          <button onClick={() => setImagePreview(null)}>✕</button>
+        </div>
+      )}
+      
+      <button className="attach-btn" onClick={handlePickImage}>
+        📎
+      </button>
+
       <input
         value={text}
         onChange={(e) => setText(e.target.value)}
@@ -25,6 +57,15 @@ export default function Input({ onSend }) {
         }
       />
       <button onClick={handleSend} className="send-btn">➤</button>
+
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleImageChange}
+          hidden
+        />  
+
     </div>
   );
 }
